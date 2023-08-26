de97c80e2f86bbfb0f833b5639f0f48c29a2558c
Richard Neumann <mail@richard-neumann.de>
2023-08-26 23:32:18 +0200

    Use random XP values

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,14 @@
 package org.example;
 
+import static java.util.concurrent.ThreadLocalRandom.current;
+
 public class Main {
     public static void main(String[] args) {
-        Character link = new Character("Link");
+        Character link = new Character("Link", current().nextInt());
         System.out.println(link);
-        Character acheman = new Character("Acheman");
+        Character acheman = new Character("Acheman", current().nextInt());
         System.out.println(acheman);
-        link.gainXp(42);
+        link.gainXp(current().nextInt());
         System.out.println(link);
         CombatResult result = link.combat(acheman);
         System.out.printf("%s %s %s %s\n", link.getName(), result, "against", acheman.getName());
