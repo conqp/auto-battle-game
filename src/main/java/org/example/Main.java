9a42fb75f89a4bf01ee641c5920905bdd16dff54
Richard Neumann <mail@richard-neumann.de>
2023-08-26 23:35:24 +0200

    Limit initial and gained XP to 0..100

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,11 +4,11 @@ import static java.util.concurrent.ThreadLocalRandom.current;
 
 public class Main {
     public static void main(String[] args) {
-        Character link = new Character("Link", current().nextInt());
+        Character link = new Character("Link", current().nextInt(0, 100));
         System.out.println(link);
-        Character acheman = new Character("Acheman", current().nextInt());
+        Character acheman = new Character("Acheman", current().nextInt(0, 100));
         System.out.println(acheman);
-        link.gainXp(current().nextInt());
+        link.gainXp(current().nextInt(0, 100));
         System.out.println(link);
         CombatResult result = link.combat(acheman);
         System.out.printf("%s %s %s %s\n", link.getName(), result, "against", acheman.getName());
