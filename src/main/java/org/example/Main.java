f5edd6f1b97c79a986e30d172b08cebd5e3488db
Richard Neumann <mail@richard-neumann.de>
2023-08-27 22:16:09 +0200

    Use instance of ThreadLocalRandom

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,14 +1,15 @@
 package org.example;
 
-import static java.util.concurrent.ThreadLocalRandom.current;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Main {
     public static void main(String[] args) {
-        Character link = new Character("Link", current().nextInt(0, 100));
+        ThreadLocalRandom random = ThreadLocalRandom.current();
+        Character link = new Character("Link", random.nextInt(0, 100));
         System.out.println(link);
-        Character acheman = new Character("Acheman", current().nextInt(0, 100));
+        Character acheman = new Character("Acheman", random.nextInt(0, 100));
         System.out.println(acheman);
-        link.gainXp(current().nextInt(0, 100));
+        link.gainXp(random.nextInt(0, 100));
         System.out.println(link);
         CombatResult result = link.combat(acheman);
         System.out.printf("%s %s %s %s\n", link.getName(), result, "against", acheman.getName());
