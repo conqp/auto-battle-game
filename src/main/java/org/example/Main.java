5c00cb518157095c0e820889e7589cf19c7b4613
Richard Neumann <mail@richard-neumann.de>
2023-08-27 22:16:22 +0200

    Add some newlines

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,12 +5,16 @@ import java.util.concurrent.ThreadLocalRandom;
 public class Main {
     public static void main(String[] args) {
         ThreadLocalRandom random = ThreadLocalRandom.current();
+
         Character link = new Character("Link", random.nextInt(0, 100));
         System.out.println(link);
+
         Character acheman = new Character("Acheman", random.nextInt(0, 100));
         System.out.println(acheman);
+
         link.gainXp(random.nextInt(0, 100));
         System.out.println(link);
+        
         CombatResult result = link.combat(acheman);
         System.out.printf("%s %s %s %s\n", link.getName(), result, "against", acheman.getName());
     }
